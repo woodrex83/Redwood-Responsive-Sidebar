d5052b6c48cd427964864e2ad00ea13a633341da
Rex Mok <rex_mok@infrasys.com.hk>
2022-11-03 10:12:52 +0800

    added react-responsive

--- web/src/layouts/CommonLayout/CommonLayout.js
+++ web/src/layouts/CommonLayout/CommonLayout.js
@@ -1,34 +1,19 @@
 import { Link, routes } from "@redwoodjs/router"
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import { useMediaQuery } from "react-responsive"
 
 const CommonLayout = ({ children }) => {
   // dropdown only appear in portrait screen
   const [dropdown, setDropdown] = useState(false)
 
-  // check is desktop or laptop screen
-  const [isDesktop, setIsDesktop] = useState(false)
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 767) {
-        setIsDesktop(false);
-        // keep dropdown not being buggy when resizing browser
-        setDropdown(false);
-      }
-      else if (window.innerWidth > 768) {
-        setIsDesktop(true);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    // call handler so state gets updated with inital window size
-    handleResize();
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [])
+  const Desktop = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 768 })
+    return isDesktop ? children : null
+  }
+  const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+    return isMobile ? children : null
+  }
 
   const menuItems = [
     { id: "1", title: "Dashboard", style: "bi bi-clipboard-data mx-2", to: "" },
@@ -40,61 +25,62 @@ const CommonLayout = ({ children }) => {
   return (
     <div className="mx-4">
       <header className="items-center justify-between border-b py-2 flex">
-        {isDesktop ? (
-            <Link to={routes.home()}>
-              <h1 className="text-xl font-normal leading-none md:py-3">
-                Simple Backend
-              </h1>
-            </Link>
-          ) : (
-            <div id="hide-btn">
-              <div className="flex items-center justify-between py-1 md:py-3 md:block">
-                <div className="md:hidden">
-                  <button
-                    className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                    onClick={() => setDropdown(!dropdown)}
-                  >
-                    {dropdown ? (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-6 h-6"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-6 h-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                      >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M4 6h16M4 12h16M4 18h16"
-                        />
-                      </svg>
-                    )}
-                  </button>
-                </div>
+        <Desktop>
+          <Link to={routes.home()}>
+            <h1 className="text-xl font-normal leading-none md:py-3">
+              Simple Backend
+            </h1>
+          </Link>
+        </Desktop>
+        <Mobile>
+          <div id="hide-btn">
+            <div className="flex items-center justify-between py-1 md:py-3 md:block">
+              <div className="md:hidden">
+                <button
+                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setDropdown(!dropdown)}
+                >
+                  {dropdown ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  )}
+                </button>
               </div>
             </div>
-          )}
+          </div>
+        </Mobile>
       </header>
       <main className="mt-2 md:flex px-2 pb-0">
         <aside className={`md:block md:w-72 vh ${dropdown ? "sidebar-open" : "sidebar-closed"}`}>
           <nav>
-            <ul>
+            <ul className="w-48">
               {menuItems.map((item) => (
-                <li className="mt-6">
+                <li className="mt-6 w-48" key={item.id}>
                   <Link
                     to={item.to}
                     className="font-normal text-emerald-500 hover:text-emerald-400"
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -8,14 +8,14 @@ const HomePage = () => {
       <MetaTags title="Home" description="Home page" />
 
       <div className="max-w-md transition-all duration-300 mt-6 mx-auto w-full">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <div className="bg-white py-8 shadow sm:rounded-lg sm:px-10">
           <div className="flex justify-center text-2xl font-medium max-h-8 max-w-sm mx-auto w-full h-full mb-3">
             <img src={logo} alt="RedwoodJS" className="object-scale-down max-w-full max-h-full w-auto h-10 mx-auto" />
           </div>
-          <h1 class="mt-4 text-xl font-extrabold text-gray-900">
+          <h1 class="mt-4 text-center text-xl font-extrabold text-gray-900">
               Redwood Responsive Sidebar Demo
           </h1>
-          <p class="mt-2 text-sm text-gray-600 max-w">
+          <p class="mt-2 text-center text-sm text-gray-600">
             Check different size of screen in Chrome by clicking F12
           </p>
         </div>
